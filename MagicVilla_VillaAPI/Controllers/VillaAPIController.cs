587a3825114e787712694a26b6b3f01d08e5195c
Jorge Pérez Bárcenas <jpbarcenas.contractor@gmail.com>
2023-03-01 16:47:11 -0300

    - Adding Response Types - Adding Create Villa EndPoint

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_VillaAPI.Model;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Model;
 using MagicVilla_VillaAPI.Model.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,13 +10,47 @@ namespace MagicVilla_VillaAPI.Controllers
 	public class VillaAPIController : ControllerBase
 	{
 		[HttpGet]
-		public IEnumerable<VillaDTO> GetVillas()
+		public ActionResult<IEnumerable<VillaDTO>> GetVillas()
 		{
-			return new List<VillaDTO>()
-			{ 
-				new VillaDTO{Id=1,Name="Pool View"},
-				new VillaDTO{Id=2,Name="Beach View"}
-			};
+			return Ok(VillaStore.villaList);
+		}
+
+		[HttpGet("{id:int}")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public ActionResult<VillaDTO> GetVilla(int id)
+		{
+			if (id == 0)
+			{
+				return BadRequest();
+			}
+			var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+			if (villa == null)
+			{
+				return NotFound();
+			}
+			return Ok(villa);
+		}
+
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+		{
+			if (villaDTO == null)
+			{
+				return BadRequest(villaDTO);
+			}
+			if (villaDTO.Id > 0)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError);
+			}
+			villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+			VillaStore.villaList.Add(villaDTO);
+
+			return Ok(villaDTO);
 		}
 	}
 }
