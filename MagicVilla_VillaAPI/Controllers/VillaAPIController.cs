41240adb3ad5709a9fbbc2447960b73115d971e8
Jorge Pérez Bárcenas <jpbarcenas.contractor@gmail.com>
2023-03-01 16:18:32 -0300

    - Adding Villa API Controller - Adding Villa Model - Adding Villa DTO

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_VillaAPI.Model;
+using MagicVilla_VillaAPI.Model.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+	[Route("api/VillaAPI")]
+	[ApiController]
+	public class VillaAPIController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<VillaDTO> GetVillas()
+		{
+			return new List<VillaDTO>()
+			{ 
+				new VillaDTO{Id=1,Name="Pool View"},
+				new VillaDTO{Id=2,Name="Beach View"}
+			};
+		}
+	}
+}
