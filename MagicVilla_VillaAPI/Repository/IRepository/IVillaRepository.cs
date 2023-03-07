094ebde5e862b933bf1207f77a1997d84bce3334
Jorge Pérez Bárcenas <jpbarcenas.contractor@gmail.com>
2023-03-07 18:58:14 -0300

    - Adding Villa Generic Repository - Adding Villa Repository Interface - Adding Villa Repository Implementation - Adding Villa CRUDS Repository Methods

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+	public interface IVillaRepository
+	{
+		Task Create(Villa entity);
+		Task<List<Villa>> ReadAll(Expression<Func<Villa, bool>> filter = null);
+		Task<Villa> Read(Expression<Func<Villa, bool>> filter = null, bool tracked = true);
+		Task Update(Villa entity);
+		Task Delete(Villa entity);
+		Task Save();
+	}
+}
---
+++ MagicVilla_VillaAPI/Repository/VillaRepository.cs
@@ -0,0 +1,70 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+	public class VillaRepository : IVillaRepository
+	{
+		private readonly ApplicationDbContext _dbo;
+
+        public VillaRepository(ApplicationDbContext dbo)
+        {
+			_dbo = dbo;
+        }
+        public async Task Create(Villa entity)
+		{
+			await _dbo.Villas.AddAsync(entity);
+			await Save();
+		}
+
+		public async Task<Villa> Read(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
+		{
+			IQueryable<Villa> query = _dbo.Villas;
+
+			if (!tracked)
+			{
+				query = query.AsNoTracking();
+			}
+
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
+
+			return await query.FirstOrDefaultAsync();
+		}
+
+		public async Task<List<Villa>> ReadAll(Expression<Func<Villa, bool>> filter = null)
+		{
+			IQueryable<Villa> query = _dbo.Villas;
+
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
+
+			return await query.ToListAsync();
+		}
+
+		public Task Update(Villa entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task Delete(Villa entity)
+		{
+			_dbo.Villas.Remove(entity);
+			await Save();
+		}
+
+		public async Task Save()
+		{
+			await _dbo.SaveChangesAsync();
+		}
+	}
+}
